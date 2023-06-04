38f5e7ad663c8f8bd3b914add30d6cdc25e84516
jamblet <fumba.yolokazi@gmail.com>
2023-06-04 12:05:01 +0200

    showing the current date

--- src/index.js
+++ src/index.js
@@ -2,9 +2,52 @@ let apiKey = "d2ea80f6e7c49d7345f579b725babe1e";
 let city = "Cape Town";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q= ${city}&appid=${apiKey}&units=metric`;
 
+function formatDate(timestamp) {
+  let time = new Date(timestamp);
+  let hours = time.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[time.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[time.getMonth()];
+  let date = time.getDate();
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  return `${day} ${month} ${date} ${hours}:${minutes} `;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#number");
-  console.log(response);
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -18,5 +61,7 @@ function displayTemperature(response) {
   maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 axios.get(apiUrl).then(displayTemperature);
