35f4cfbd11243428f85ee50f086a7ed4bdcc8e2a
jamblet <fumba.yolokazi@gmail.com>
2023-06-10 08:05:09 +0200

    added the getForecast function

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function formatDate(timestamp) {
   return `${day} ${month} ${date} ${hours}:${minutes} `;
 }
 
-function displayforecast() {
+function displayforecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -66,6 +66,12 @@ function displayforecast() {
 
   forecastHTML = forecastHTML + "</div>";
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "d2ea80f6e7c49d7345f579b725babe1e";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -93,6 +99,8 @@ function displayTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
